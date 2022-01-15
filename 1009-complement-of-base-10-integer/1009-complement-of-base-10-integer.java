1f0aa4157c9c5353df1e618dd3acb1f09a72251c
Akash Ghanate <akashghanate98@gmail.com>
2022-01-15 21:27:33 +0530

    Time: 0 ms (100.00%), Space: 37.8 MB (38.42%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
@@ -7,7 +7,7 @@ class Solution {
         int result=n;
         while (n!=0){
             n=n>>1;
-            mask=(mask<<1)|1;
+            mask=(mask<<1)|1; // mask creates 32 bit number with set bits as of in n
         }
         result = mask&(~result);
         return result;
