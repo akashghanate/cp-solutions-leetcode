e5f81614b000c5f31a1bdaf3c76e588b84a65a55
Akash Ghanate <akashghanate98@gmail.com>
2022-02-13 23:34:13 +0530

    Time: 1 ms (95.85%), Space: 52.5 MB (5.46%) - LeetHub

--- 605-can-place-flowers/605-can-place-flowers.java
+++ 605-can-place-flowers/605-can-place-flowers.java
@@ -3,6 +3,7 @@ class Solution {
         if(n==0)
             return true;
         
+        // check if 3 consecutive elements are 0, consider i-1 and legth+1 as 0
         for(int i=0;i<flowerbed.length;i++){
             if(flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && (i == flowerbed.length - 1 || flowerbed[i + 1] == 0)){
                 n--;
