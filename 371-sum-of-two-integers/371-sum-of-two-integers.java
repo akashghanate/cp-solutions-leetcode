59093b74aaa9bc85e6de3e4492b80d57d823af3e
Akash Ghanate <akashghanate98@gmail.com>
2022-03-06 18:33:42 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (40.68%) - LeetHub

---
+++ 371-sum-of-two-integers/371-sum-of-two-integers.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int getSum(int a, int b) {
+        // cannot use +,-
+        // but XOR can be used bitwise to replicate addition of bit
+        // carry can be calculated using bit & 
+        
+        // Logic:
+        // a^b : for base
+        // a&b << 1 : carry for each bit
+         
+        // T.C: O(1)
+        
+        // using b for carry and at last carry will be 0
+        while(b!=0){
+            int temp = (a & b) << 1; 
+            a = a ^ b;
+            b = temp;
+        }
+        
+        return a;
+    }
+}
